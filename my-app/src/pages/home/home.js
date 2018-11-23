a93a4b61c7887bce09433037e05a250e42c8ab9b
sarinhamartins <sariinha.martins@gmail.com>
2018-11-23 09:39:04 -0200

    criação do input da home

--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -3,6 +3,7 @@ import { getUser } from '../../infra/local-storage'
 import { Redirect } from 'react-router-dom'
 import Postit from '../../components/postit'
 import { getPostitsApi } from '../../apis/postit.api'
+import './home.css'
 
 class Home extends React.Component {
     constructor() {
@@ -37,17 +38,20 @@ class Home extends React.Component {
         if (user) {
             return (
                 <div className='home'>
-                    <Postit updatePostits={this.getPostits}/>
-                    {this.state.postits.map((item, index) => (
-                        <Postit
-                            key = {item._id}
-                            id = {item._id}
-                            title = {item.title}
-                            text = {item.desc}
-                            color = {item.color}
-                            updatePostits={this.getPostits}
-                        />
-                    ))}
+                <input type='text'  className='home__search' />
+                    <div>
+                        <Postit updatePostits={this.getPostits}/>
+                        {this.state.postits.map((item, index) => (
+                            <Postit
+                                key = {item._id}
+                                id = {item._id}
+                                title = {item.title}
+                                text = {item.desc}
+                                color = {item.color}
+                                updatePostits={this.getPostits}
+                            />
+                        ))}
+                    </div>
                 </div>
             )
         } else {
